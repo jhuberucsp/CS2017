a25d128b192df14699efcda1cad6a0fba11a5323
ALUMNO-D <ALUMNO-D@UCSP.AULAB>
2017-08-24 09:55:45 -0500

    Actualizacion codigo

---
+++ hello/temperature.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<iomanip> //manipulacioin de out put
+using namespace std;
+/*imprimir tabla de gerados*/
+int main(){
+    bool far,celsius;
+    int lower upper,step;
+
+    fahr=lower;
+    }
+ /*En este pro grama se convertira de Grados Fahrenheid  a Celcius usaremos la formula ##### """"""C=(F-32)*5/9"""" ########*/
+int main(){
+    float C;
+    cout<<"Este programa lo ayudara proporcionandole una tabla con los grados fahrenheit a celcius"<<endl;
+    for(float i=0;i<301;i=i+20){// 20 es el far 301 upper lower 0
+            C =(i-32)*5/9;
+            cout<<i<<" F"<<"<==>"<<C<<" C"<<endl;
+
+    }
+
+return 0;}
