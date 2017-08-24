3b29fe47f3c1d69dc7edfde0fc6824b70dbe8be8
ALUMNO-D <ALUMNO-D@UCSP.AULAB>
2017-08-24 09:37:11 -0500

    Actualizacion codigo

--- hello/primo.cpp
+++ hello/primo.cpp
@@ -1,10 +1,41 @@
 #include<iostream>
 using namespace std;
- int main(){
-    int num,cont;
-    cout<<"Ingrexe numero"<<std;
+/* bool main(){
+    int num;
+    bool cont;
+
+    cout<<"Ingrese numero"<<endl;
     cin>>num;
-    con=0,
-    for(int i=2;num/2;i++){
-        if((num%i)==0)
-    }}
+    cont=true;
+    for(int i=2;num-1;i++){
+        if((num%i)==0){
+                cont=false;
+        }}
+    if(cont==true){cout<<"es primo"<<endl;}
+    else
+        cout<<"no es primo"<<endl;}*/
+
+
+int main(){
+    int num;
+    cout<<"Ingrese numero"<<endl;
+    cin>>num;
+    bool c=true;
+    int n=1;
+    while((c==true||n<num)&&num!=1){
+            n=n+1;
+            if(num%n==0){
+                c=false;
+                cout<<"no es primo"<<endl;
+                return 0;
+            }
+            else
+                cout<<"es primo"<<endl;
+                return 0;
+
+
+    }
+    return 0;
+    }
+
+
